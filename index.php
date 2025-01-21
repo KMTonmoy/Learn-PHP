1a617236f35bd9894c8b599b044132ee02b36fc0
KMTonmoy <tonmoyahamed2009@gmail.com>
2025-01-21 08:54:43 +0600

    Some HTML Added

--- index.php
+++ index.php
@@ -4,13 +4,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title> <?php echo "This Is Index.PHP";
+            ?></title>
 </head>
 
 <body>
-
-    <?php echo "This Is Tonmoy.PHP";
+    <?php echo "This Is Index.PHP";
     ?>
+
 </body>
 
 </html>
