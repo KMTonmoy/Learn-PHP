394b16e9bbb79806e9c3e80ed32bdf9abf7f11c6
KMTonmoy <tonmoyahamed2009@gmail.com>
2025-01-21 10:07:53 +0600

    5 No Video Compleated

---
+++ CWTPHP/05_varuables.php
@@ -0,0 +1,12 @@
+<?php
+// echo "This is Tutorial 5"
+
+// Variables are Containers for Storing Information
+$name = "Tonmoy";
+$income = 500;
+echo "This Guy is $name & His Income is BDT $income  </br>";
+
+echo "$name is a Good Boy </br>";
+echo "$name Is the Real Ganagstar </br>";
+
+ 
